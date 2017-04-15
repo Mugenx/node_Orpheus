3719a428bca2842d35a5b961bd0cf720aa20c698
Mugenx <sahawu.mail@gmail.com>
2017-04-14 23:24:38 -0400

    framework created, using MySQL

---
+++ app/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var reload = require('reload');
+var app = express();
+
+app.set('port', process.env.PORT || 4000);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+
+app.locals.siteTitle = 'ALMS';
+
+app.use(express.static('app/public'));
+app.use(require('./routes/index'));
+
+
+var server = app.listen(app.get('port'), function () {
+    console.log('GO to http://localhost:' + app.get('port'));
+});
+
+reload(server, app);
---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var db = require('../database.js');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+
+    db.query('select username, firstname, lastname from users', function (err, results) {
+        if (err) throw err;
+
+        res.render('index', {
+            pageTitle: "Home",
+            pageID: 'home',
+            results: results
+        })
+        ;
+    });
+
+
+});
+
+module.exports = router;
